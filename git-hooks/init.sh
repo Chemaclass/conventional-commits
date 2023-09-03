62e0e5a60c2227b1775153a410b980b00f1e5330
Chemaclass <chemaclass@outlook.es>
2023-09-03 19:44:49 +0200

    !(test) commit message

---
+++ git-hooks/init.sh
@@ -0,0 +1,12 @@
+#!/usr/bin/env bash
+
+set -e
+
+function setup_git_hooks()
+{
+  echo "Initialising git hooks..."
+  ln -sf "./git-hooks/prepare-commit-msg.sh" ".git/hooks/prepare-commit-msg"
+  echo "Done"
+}
+
+setup_git_hooks
